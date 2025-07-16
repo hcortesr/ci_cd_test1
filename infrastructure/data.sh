8fa728d1a0d1ea128814059d35530ac8f3bba2a8
Hector <hectorcortesr04@gmail.com>
2025-07-15 21:28:44 -0600

    Dockerized app

--- infrastructure/data.sh
+++ infrastructure/data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-apt update -y
-apt install -y apache2
-systemctl start apache2
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+systemctl enable docker
+systemctl start docker
